1c8fee7420f92823e1c561d0804be685cbdcfc98
Hileene <92981899+Hileene@users.noreply.github.com>
2024-01-14 00:40:05 +0100

    Mise en place du fonctionnement du slider

--- changeLang.js
+++ changeLang.js
@@ -180,3 +180,28 @@ document
 
 // Mise à jour de l'email via l'objet "config"
 document.querySelector('#email-info').innerHTML = config.email
+
+//Slider
+
+var slide_index = 1
+displaySlides(slide_index)
+function nextSlide(n) {
+  displaySlides((slide_index += n))
+}
+function currentSlide(n) {
+  displaySlides((slide_index = n))
+}
+function displaySlides(n) {
+  var i
+  var slides = document.getElementsByClassName('showSlide')
+  if (n > slides.length) {
+    slide_index = 1
+  }
+  if (n < 1) {
+    slide_index = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none'
+  }
+  slides[slide_index - 1].style.display = 'block'
+}
