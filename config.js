197720f6d153bc8394b545ac9c65b412e527572a
Hileene <92981899+Hileene@users.noreply.github.com>
2024-01-15 13:24:59 +0100

    Suppression du fichier config

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-const config = {
-  email: process.env.EMAIL || 'fallback@example.com',
-}
-
-// Permet d'exporter l'objet "config" et de le rendre disponible à d'autres scripts
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = config
-}
